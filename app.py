ddde548cd96754f7a6e181e6d82ba70b061ee320
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 21:07:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,31 @@ def set_png_as_page_bg(png_file):
 set_png_as_page_bg('ottok_klientov.jpg')
 
 
+st.markdown(""" 
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+""", unsafe_allow_html=True)
+
 classifier_name=['Random Forest']
 option = st.sidebar.selectbox('Алгоритм', classifier_name)
 st.subheader(option)
