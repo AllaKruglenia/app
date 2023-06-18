b7d5d82f1e3482574e1618f46abce8dfd60be9ef
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 19:45:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ if st.button("Предсказать отток клиентов"):
 st.sidebar.title('ИТ-АКАДЕМИЯ ПРИОРБАНК')
 st.sidebar.title('Проект "Отток клиентов"')
 
-st.sidebar.info('Курс Diving into Darkness of Data Science.')
-st.sidebar.info('Подготовила проект Кругленя А.М.')      
+st.sidebar.text('Курс Diving into Darkness of Data Science.')
+st.sidebar.text('Подготовила проект Кругленя А.М.')      
 
 
