038102d10fc034b65e662e222e8f0c2e0218731f
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 20:30:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
 def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
     input = np.array([[Balance, EstimatedSalary]]).astype(np.float64)
-    if option == 'XGBoost':
+    if option == 'Random Forest':
         prediction = model.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
     return float(pred)    
