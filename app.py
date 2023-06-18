0e90c193db9c1c30daa3fef3e04348079e21f26f
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 21:23:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,34 +29,12 @@ def set_png_as_page_bg(png_file):
     return
 set_png_as_page_bg('ottok_klientov.jpg')
 
-
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
-    background-color: #ce1126;
-    color: white;
-    height: 3em;
-    width: 12em;
-    border-radius:10px;
-    border:3px solid #000000;
-    font-size:20px;
-    font-weight: bold;
-    margin: auto;
-    display: block;
+    background-color: rgb(90, 90, 90);
 }
-
-div.stButton > button:hover {
-	background:linear-gradient(to bottom, #ce1126 5%, #ff5a5a 100%);
-	background-color:#ce1126;
-}
-
-div.stButton > button:active {
-	position:relative;
-	top:3px;
-}
-
 </style>""", unsafe_allow_html=True)
-
 
 
 
